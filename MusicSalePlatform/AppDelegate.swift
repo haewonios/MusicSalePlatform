e1fc2020bd0fb4c962159bbd219ce2e6d573cfd0
haewonios <haewon.dev@gmail.com>
2025-10-27 00:59:41 +0900

    initial commit

---
+++ MusicSalePlatform/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  MusicSalePlatform
+//
+//  Created by hyewon on 10/27/25.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                
+        return true
+    }
+}
---
+++ MusicSalePlatform/MusicSalePlatformApp.swift
@@ -0,0 +1,19 @@
+//
+//  MusicSalePlatformApp.swift
+//  MusicSalePlatform
+//
+//  Created by hyewon on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicSalePlatformApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

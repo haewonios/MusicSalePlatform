aab5d28f1bb45283767be085f407063fd5027ec7
haewonios <haewon.dev@gmail.com>
2025-11-11 00:03:37 +0900

    TCA 라이브러리 추가

---
+++ MusicSalePlatform/Presentation/Home/HomeFeature.swift
@@ -0,0 +1,24 @@
+//
+//  HomeFeature.swift
+//  MusicSalePlatform
+//
+//  Created by hyewon on 11/10/25.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct HomeFeature {
+    @ObservableState
+    struct State: Equatable {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some Reducer<State, Action> {
+        
+    }
+}
